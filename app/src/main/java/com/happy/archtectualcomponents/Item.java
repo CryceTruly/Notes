7517bb47a93a70e0298dc0b7bc630ea01ee29878
crycetruly <crycetruly@gmail.com>
2019-02-14 20:41:55 +0300

    Created Item Entity

---
+++ app/src/main/java/com/happy/archtectualcomponents/Item.java
@@ -0,0 +1,61 @@
+package com.happy.archtectualcomponents;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "item")
+public class Item {
+    private String title,description;
+    private Boolean completed;
+    private int priority;
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    public Item(String title, String description, Boolean completed, int priority) {
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+        this.priority = priority;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
